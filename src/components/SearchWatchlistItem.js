667f497f121f9fcee39aa034bdbb37ca48a27f39
bexi <trreitmaier@msn.com>
2020-04-06 14:21:33 +0200

    first version of search field, shows title, imdb rating and movie description

---
+++ src/components/SearchWatchlistItem.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import {makeStyles} from "@material-ui/core/styles";
+import {fade} from "@material-ui/core/styles/colorManipulator";
+import {IMDB_GET} from "../utils/imdb-api-utils";
+import {API_POST} from "../utils/api-utils";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+    centerContent: {
+        marginTop: theme.spacing(2),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    dropdown: {
+        backgroundColor: fade('rgb(255,255,255)', 0.9),
+        [theme.breakpoints.down('sm')]: {
+            width:'10%',
+        },
+        [theme.breakpoints.up('md')]: {
+            width:'10%',
+        },
+        [theme.breakpoints.up('lg')]: {
+            width:'50%',
+        },
+    },
+    dropdownItem: {
+        backgroundColor: fade(theme.palette.secondary.main, 0)
+    },
+    watchlistSearch: {
+        width:'100%',
+        paddingTop: theme.spacing(2),
+    },
+}));
+
+const SearchWatchlistItem = ({ updateWatchlist }) => {
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchItem, setSearchItem] = useState('');
+
+    const classes = useStyles();
+
+    const searchImdb = () => {
+        const url = 'https://api.themoviedb.org/4/search/movie?&language=en-US&query='
+        IMDB_GET(`https://api.themoviedb.org/4/search/movie?&language=en-US&query=${searchItem}`).then((res) => {
+            console.log(res);
+            setSearchResults(res.data.results.slice(0,5));
+        });
+    }
+
+    const addWatchlistItemToDB = async(title) => {
+        console.log('add item to watchlist');
+        try {
+            await API_POST('/usermovies', { content: title });
+            updateWatchlist();
+            setSearchItem('');
+            setSearchResults([]);
+        } catch (e) {
+            alert(e);
+        }
+    }
+
+    const renderSearchResults = () => (
+        <Box  position="relative" style={{width:'100%'}}>
+            { searchResults.length>0 && <Box
+                position="absolute"
+                zIndex="modal"
+                className={classes.dropdown}
+                borderRadius="borderRadius"
+                style={{width:'100%', padding:'1%'}}
+            >
+                {searchResults.map((item) => {
+                    let posterUrl =  `https://image.tmdb.org/t/p/original/${item['poster_path']}`
+                    return(
+                        <Box
+                            bgcolor="background.paper"
+                            color="text.primary"
+                            p={2}
+                            onClick={() => addWatchlistItemToDB(item.title)}
+                            className={classes.dropdownItem}
+                            borderRadius="borderRadius"
+                        >
+                            <Grid container >
+                                <Grid item xs={3} > <img style={{width:'30%'}} src={posterUrl} /></Grid>
+                                <Grid item xs={9} >
+                                    <Grid container >
+                                        <Grid xs={12}>
+                                            <Typography component="h5" variant="h5">
+                                                {item.title}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid xs={12}>
+                                            <b>Imdb: {item['vote_average']}</b>
+                                        </Grid>
+                                        <Grid xs={12}>
+                                            Description: {item['overview']}
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    )})}
+            </Box>}
+        </Box>
+    );
+
+    return (
+        <>
+            <div className={classes.centerContent}  >
+                <Typography component="h1" variant="h5">
+                    Watchlist
+                </Typography>
+                <TextField
+                    id="watchlist-search"
+                    variant="outlined"
+                    className={classes.watchlistSearch}
+                    value={searchItem}
+                    onChange={(e) => {
+                        setSearchItem(e.target.value);
+                        if(searchItem.length>4){
+                            searchImdb();
+                        }else{
+                            setSearchResults([]);
+                        }
+                    }
+                    }
+                />
+            </div>
+            {renderSearchResults()}
+        </>
+    );
+}
+
+export default SearchWatchlistItem;
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,15 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import axios from 'axios';
 
+import { fade } from '@material-ui/core/styles/colorManipulator';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 //import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
+
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Box from '@material-ui/core/Box';
 
 import {API_GET} from '../utils/api-utils'
 import AddWatchlistItem from '../components/AddWatchlistItem';
 import WatchlistGrid from './WatchlistGrid';
 import {IMDB_GET} from '../utils/imdb-api-utils';
+import SearchWatchlistItem from '../components/SearchWatchlistItem';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -26,6 +33,10 @@ const useStyles = makeStyles((theme) => ({
 export default (props) => {
   const [watchlistItems, setWatchlistItems] = useState([]);
   const [initialLoad, setInitialLoad] = useState(true);
+  //const [searchItem, setSearchItem] = useState('');
+  //const [searchResults, setSearchResults] = useState([]);
+  const [image, setImage] = useState(null);
+
   /*const [state, setState] = useState({
     checkedA: true,
     checkedB: true,
@@ -41,10 +52,6 @@ export default (props) => {
     }
     onLoad();
   }, [props.isAuthenticated]);
-
-  const loadWatchList = () => {
-    return API_GET("/usermovies");
-  }
 
   const updateWatchlist = async() => {
     try {
@@ -84,14 +91,10 @@ export default (props) => {
     </Grid>
   );*/
 
-
-  // Send request to the movie db   
-  //IMDB_GET('https://api.themoviedb.org/4/search/movie?&language=en-US&query=secret%20life&page=1&include_adult=false' ).then((res) => console.log(res));
-
   const MainContent = (
     <Container component="main" maxWidth="md">
       <CssBaseline />
-      <AddWatchlistItem updateWatchlist={updateWatchlist}/>
+      <SearchWatchlistItem updateWatchlist={updateWatchlist} />
       <div className={classes.paper}>
         <WatchlistGrid
             watchlistItems={watchlistItems}
@@ -100,6 +103,5 @@ export default (props) => {
       </div>
     </Container>
   );
-
   return ( props.isAuthenticated ? MainContent : <Redirect to='/login'/>);
 }
